9ba04970b4559cc8ec76174d475cf4e11525050a
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-09-24 17:28:52 +0200

    v3.1.b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
     def set_layout_variables
       @ctrl = params[:controller]
       @sitename ||= request.subdomains.any? ? request.subdomains(0).first.upcase : "IAE-Planning DEV"
-      @sitename.concat(" v3.1.a")
+      @sitename.concat(" v3.1.b")
 
       if current_user
         @cours_params = {}

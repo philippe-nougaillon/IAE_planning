592c55e2703eee3d0f521c0f9ac72d31f89ec853
philippe-nougailon <philippe.nougaillon@gmail.com>
2019-11-04 14:14:57 +0100

    Routes simplifiées

--- config/routes.rb
+++ config/routes.rb
@@ -1,78 +1,83 @@
 Rails.application.routes.draw do
   
-  get 'guide/index'
+  devise_for :users
+  
+  resources :cours do
+    collection do
+      get :planning, to: "cours#index_slide"
 
+      post :action
+      post :action_do
+    end
+  end
+    
+  resources :salles do
+      collection do
+        get :occupation
+      end
+  end
+
+  resources :formations
+  resources :intervenants
+  resources :users
+  resources :fermetures
+  resources :documents
+  resources :etudiants
+ 
   resources :import_logs do
     member do
       get 'download_imported_file'
     end
   end
-
-  resources :etudiants
   
-  get 'tools/index'
-  get 'tools/import'
-  post 'tools/import_do'
-  get 'tools/creation_cours'
-  post 'tools/creation_cours_do'
-  get 'tools/import_intervenants'
-  post 'tools/import_intervenants_do'
-  get 'tools/import_utilisateurs'
-  post 'tools/import_utilisateurs_do'
-  get 'tools/export'
-  post 'tools/export_do'
-  get 'tools/export_intervenants'
-  post 'tools/export_intervenants_do'
-  get 'tools/swap_intervenant'
-  post 'tools/swap_intervenant_do'
-  get 'tools/etats_services'
-  get 'tools/audits'
-  get 'tools/import_etudiants'
-  post 'tools/import_etudiants_do'
-  get 'tools/export_etudiants'
-  post 'tools/export_etudiants_do'
-  get 'tools/taux_occupation_jours'
-  post 'tools/taux_occupation_jours_do'
-  get 'tools/taux_occupation_salles'
-  post 'tools/taux_occupation_salles_do'
+  namespace :tools do
+    get :index
 
-  get 'tools/nouvelle_saison'
-  post 'tools/nouvelle_saison_create'
+    get :import
+    get :import_intervenants
+    get :import_utilisateurs
+    get :import_etudiants
+    get :export
+    get :export_intervenants
+    get :export_etudiants
 
-  get 'tools/notifier_intervenants'
-  post 'tools/notifier_intervenants_do'
+    get :swap_intervenant
+    get :etats_services
+    get :audits
+    get :taux_occupation_jours
+    get :taux_occupation_salles
+    get :nouvelle_saison
+    get :notifier_intervenants
+    get :creation_cours
 
-  get 'salles/occupation'
-  get 'salles/occupation', as: :occupation_des_salles
+    post :import_do
+    post :creation_cours_do
+    post :import_intervenants_do
+    post :import_utilisateurs_do
+    post :import_etudiants_do
+    post :export_do
+    post :export_intervenants_do
+    post :export_etudiants_do
+    post :swap_intervenant_do
+    post :taux_occupation_jours_do
+    post :taux_occupation_salles_do
+    post :nouvelle_saison_create
+    post :notifier_intervenants_do
+  end
 
-  get 'cours/index_slide' # à supprimer 
-  get 'cours/planning' => "cours#index_slide"
-
-  post 'cours/action'
-  post 'cours/action_do'
-
-  devise_for :users
-  
-  resources :documents
-  resources :cours
-  resources :salles
-  resources :formations
-  resources :intervenants
-  resources :users
-  resources :fermetures
+  get 'guide/index'
   
   namespace :api, defaults: {format: 'json'} do 
     namespace :v1 do 
         resources :cours
     end 
-    namespace :v2 do 
 
+    namespace :v2 do 
         resources :cours do
           collection do
             get 'in_progress'
           end
         end
-
         resources :etudiants
     end 
   end 
@@ -81,54 +86,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
   root 'cours#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end

bec16a2a3f79a8889ddf9a005089ec87b11ff7d2
philippe-nougailon <philippe.nougaillon@gmail.com>
2020-06-26 16:55:58 +0200

    Maj mail notification nouveaux cours

--- test/mailers/previews/intervenant_mailer_preview.rb
+++ test/mailers/previews/intervenant_mailer_preview.rb
@@ -3,8 +3,8 @@ class IntervenantMailerPreview < ActionMailer::Preview
     def notifier_cours
         IntervenantMailer.notifier_cours(Date.today, 
                                         Date.today.end_of_month, 
-                                        Intervenant.first, 
-                                        Cour.all.limit(10),
+                                        Intervenant.last, 
+                                        Intervenant.last.cours.last(6),
                                         {"MBA lkjlkjlkjl": "regis@iae-paris.com", 
                                          "MBA SANTE 2019/2020": "laigle@iae-paris.com"})
     end
